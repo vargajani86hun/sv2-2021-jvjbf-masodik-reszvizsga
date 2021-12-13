73609b8f1854088ff7b0c4d4152de36918d5caa8
JaniVarga86HUN <jani.varga86@gmail.com>
2021-12-13 14:30:20 +0100

    In the 3rd task, class Office is ready.

---
+++ sv2-2021-jvjbf-masodik-reszvizsga/src/main/java/city/Office.java
@@ -0,0 +1,39 @@
+package city;
+
+
+public class Office extends Building {
+    public static final int MINIMUM_AREA_FOR_TABLE = 2;
+    public static final int MAXIMUM_AREA_FOR_TABLE = 5;
+
+    private String company;
+    private int numberOfTablesPerLevel;
+
+    public Office(double area, int levels, Address address, String company, int numberOfTablesPerLevel) {
+        super(area, levels, address);
+        this.company = company;
+        if (numberOfTablesPerLevel*MINIMUM_AREA_FOR_TABLE > area) {
+            throw new IllegalArgumentException("Too many tables per level!");
+        }
+        if (numberOfTablesPerLevel*MAXIMUM_AREA_FOR_TABLE < area) {
+            throw new IllegalArgumentException("Too few tables per level!");
+        }
+        this.numberOfTablesPerLevel = numberOfTablesPerLevel;
+    }
+
+    public Office(double area, Address address, String company, int numberOfTablesPerLevel) {
+        this(area, 1, address, company, numberOfTablesPerLevel);
+    }
+
+    @Override
+    public int calculateNumberOfPeopleCanFit() {
+        return numberOfTablesPerLevel * (getLevels() - 1);
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public int getNumberOfTablesPerLevel() {
+        return numberOfTablesPerLevel;
+    }
+}
